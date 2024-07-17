5fc435502862899b385b360c273e23e158dc18b7
khiemhuynh95 <khiemhuynh952@gmail.com>
2024-07-17 18:47:02 -0500

    turn on api, update pyspark dag to get params from api call

--- dags/dag_pyspark.py
+++ dags/dag_pyspark.py
@@ -8,20 +8,23 @@ import os
 import shutil
 
 from airflow.decorators import task
-params = {
-    "connection_id": "minio_conn",
-    "bucket_name": "airflow",
-    "bucket_key": "Orders.csv",
-    "date": "2022-06-02"
-}
+# params = {
+#     "connection_id": "minio_conn",
+#     "bucket_name": "airflow",
+#     "bucket_key": "Orders.csv",
+#     "date": "2022-06-02"
+# }
 
-minio_client = MinioHook(connection_id=params["connection_id"])
+minio_client = MinioHook(connection_id="minio_conn")
+
+def test_s3_connection():
+    minio_client.test_connection()
 
 def download_csv_file(ti, **context):
+    print(context)
     filename = context["params"]["bucket_key"].split('//')[-1]
     local_filepath = f'/tmp/{filename}'
     ti.xcom_push(key='input_file_path', value=local_filepath)
-    
     
     minio_client.download_file(context["params"]["bucket_name"], context["params"]["bucket_key"], local_filepath)
     
@@ -53,22 +56,15 @@ def process_csv_file(ti, **context):
     ti.xcom_push(key='date', value=snapshot_date)
     spark.stop()
 
-
-
 with DAG(
     dag_id='pyspark_example', 
     start_date=datetime(2022, 2, 12),
-    params=params,
     schedule_interval=None
 ) as dag:
-    sensor_s3_file_task = S3KeySensor(
-        task_id='sensor_minio_s3',
-        bucket_name=params['bucket_name'],
-        bucket_key=params['bucket_key'],
-        aws_conn_id=params['connection_id'],
-        mode='poke',
-        poke_interval=5,
-        timeout=30
+    
+    test_s3_connection_task = PythonOperator(
+        task_id='test_s3_connection',
+        python_callable=test_s3_connection
     )
 
     download_csv_file_task = PythonOperator(
@@ -91,4 +87,5 @@ with DAG(
         python_callable=remove_temp_files
     )
 
-    sensor_s3_file_task >> download_csv_file_task >> process_csv_file_task >> upload_output_file_task >> clean_up_task
+    test_s3_connection_task >> download_csv_file_task >> process_csv_file_task >> upload_output_file_task >> clean_up_task
+    
